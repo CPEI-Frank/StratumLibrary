38c493a10ac3598740af847c07d919da3f5e41c5
CryptoManiac <balthazar@yandex.ru>
2015-08-29 18:49:40 +0300

    Use nvc-cce-2.coinomi.net server for testing.

--- StratumTest/Program.cs
+++ StratumTest/Program.cs
@@ -6,18 +6,21 @@ namespace StratumTest
     using Stratum;
 	using Newtonsoft.Json.Linq;
 
-    class StratumTest : Stratum
+	class StratumWrapper : Stratum
+	{
+		public StratumWrapper(string ipAddress, int port) : base(ipAddress, port) {	}
+
+		public override void NotificationHandler(string NotificationMethod, JArray NotificationData)
+		{
+			Console.WriteLine("\nNotification: Method={0}, data={1}", NotificationMethod, NotificationData.ToString());
+		}
+	}
+
+    class StratumTest
     {
-        public StratumTest(string ipAddress, int port) : base(ipAddress, port) { }
-
-        public override void NotificationHandler(string NotificationMethod, JArray NotificationData)
-        {
-            Console.WriteLine("\nNotification: Method={0}, data={1}", NotificationMethod, NotificationData.ToString());
-        }
-
         static void Main(string[] args)
         {
-			StratumTest s = new StratumTest("192.168.1.100", 40001);
+			StratumWrapper s = new StratumWrapper("176.9.65.41", 5034);
 
             while (true)
             {
